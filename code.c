7f24c0f69bb6820ccd68427ec92110fc7fa2c79f
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2024-10-09 19:22:58 +0530

    Main initialisation

--- code.c
+++ code.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(){
+  int choice;
+  printf("Hello User..\n");
+  printf("\tDisplay\t");
+  printf("""1.Add book
+            2.Display books
+            3.Remove book
+            4.Exit""");
+  scanf("%d", &choice);
+
+  switch(choice){
+    case 1:
+      addbook();
+      break;
+
+    case 2:
+      displaybook();
+      break;
+
+    case 3:
+      removebook();
+      break;
+
+    case 4:
+      exit(0);
+      break;
+
+    default:
+      printf("Please select from above options only.");
+  }
+}
