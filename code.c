863ec2ef95a7369232c0161275aef6092b623df4
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2024-10-16 13:00:45 +0530

    addbook updated

--- code.c
+++ code.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int addbook(int *book_count, char *books[]){
+int addbook(int *book_count, char *books[], max_books){
+  if(*book_count > max_books){
+    printf("Book library is full!!\n");
+    returnn 0;
+  }
   char *name;
   printf("Enter book name: ");
   fgets(name,sizeof(name),stdin);
   books[*book_count] = (char*)malloc(sizeof(name)+1);
   strcpy(books[*book_count],name);
   *book_count++;
+  printf("Book %s",name," added succesfully.\n");
+  return 1;
 }
 
 int main(){
